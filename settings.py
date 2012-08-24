e6dbdc8a372a750a491165063fb0d9ffabf12b5e
Thiago Pagonha <thi.pag@gmail.com>
2012-08-24 00:00:33 -0300

    Adicionando dependência do módulo

--- settings.py
+++ settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = (
     'dbindexer',
     'blog',
     'psn',
+    'wiki',
 
     # djangoappengine should come last, so it can override a few manage.py commands
     'djangoappengine',
