f7d58ce7ea7bccd68c6217e603ce71042ad85330
Thiago Pagonha <thi.pag@gmail.com>
2012-09-03 00:11:49 -0300

    Mudando a referência do wsdl do serviço em .NET para o meu em Python no appengine :D

--- psn/settings.py
+++ psn/settings.py
@@ -1,4 +1,5 @@
 
-PSN_WSDL = 'http://psnapi.miguel.ms/stable/PlaystationNetworkAPI.asmx?WSDL'
+#PSN_WSDL = 'http://psnapi.miguel.ms/stable/PlaystationNetworkAPI.asmx?WSDL'
+PSN_WSDL = 'http://psnserverapi.appspot.com/PlaystationNetworkAPI?wsdl'
 
 PSN_USER = 'thi_pag'
